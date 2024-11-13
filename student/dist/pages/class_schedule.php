ccd0b7c62ff27ade28ce51df2316adccc6424726
NEYAZ NAFIZ <neyaz@aintron.com>
2024-11-13 23:25:20 +0600

    initial setup of class schedule page

--- student/dist/pages/class_schedule.php
+++ student/dist/pages/class_schedule.php
@@ -4,5 +4,19 @@ require_once 'partials/header.php';
 require_once 'partials/navbar.php';
 require_once 'partials/sidebar.php';
 ?>
-<h1> class schedule page here </h1>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+  
+</body>
+</html>
+
 <?php require_once 'partials/footer.php' ?>
--- student/dist/pages/teachers.php
+++ student/dist/pages/teachers.php
@@ -5,5 +5,8 @@ require_once 'partials/navbar.php';
 require_once 'partials/sidebar.php';
 
 ?>
+
 <h1>This is page for teachers </h1>
+
+
 <?php require_once 'partials/footer.php' ?>
